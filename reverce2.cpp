3aa0fd35434f1813d252e0f31e1662a371048fc1
Sanju chauhan <sanjuraja1805@gmail.com>
2024-09-04 15:07:07 +0530

    reverce

---
+++ reverce2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int number ;
+    int modify ;
+    int reverce = 0;
+
+    cout << "Enter a any digit number "<<"\n";
+    cin >> number;
+    while(number > 0){
+        modify = number%10;
+        reverce = reverce*10 + modify;
+        number = number/10;
+    }
+    cout << reverce;
+}
